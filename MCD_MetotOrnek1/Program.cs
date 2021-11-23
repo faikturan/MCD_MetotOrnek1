8b045b1858c86a0596bc20fbca0ad0b694fcd879
Faik Turan <turanfaik@hotmail.com>
2021-11-23 16:52:19 +0300

    Add project files.

---
+++ MCD_MetotOrnek1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_MetotOrnek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EkranaMerhabaYaz();
+            EkranaMerhabaYaz();
+            EkranaMerhabaYaz();
+            EkranaMerhabaYaz();
+            EkranaGonderilenIsimIleMerhabaYaz("faik");
+            EkranaGonderilenIsimIleMerhabaYaz("erdem");
+            EkranaGonderilenIsimIleMerhabaYaz("talha");
+
+            EkranaGonderilenIsımleKacTaneMerhabaYaz("Yakup", 5);
+
+            Console.ReadKey();
+
+        }
+
+        private static void EkranaMerhabaYaz()
+        {
+            Console.WriteLine("Merhaba");
+        }
+
+        private static void EkranaGonderilenIsimIleMerhabaYaz(string Isim)
+        {
+            Console.WriteLine("Merhaba sayın " +Isim.ToUpper());
+        }
+
+        private static void EkranaGonderilenIsımleKacTaneMerhabaYaz(string Isim, int KacTane)
+        {
+            if (KacTane > 0)
+            {
+                for (int i = 0; i <= KacTane; i++)
+                {
+                    Console.WriteLine("Merhaba sayın " +Isim.ToUpper());
+                }
+            }
+            else if(KacTane <= 0)
+            {
+                Console.WriteLine("Lütfen sıfırdan farklı bir sayı gönder");
+            }
+        }
+
+
+    }
+}
